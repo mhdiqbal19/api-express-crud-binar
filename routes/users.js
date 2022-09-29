a2881593c5bbe34bacc7516deb6b42deff449417
MUHAMMAD IQBAL <43297852+mhdiqbal19@users.noreply.github.com>
2022-09-29 21:14:47 +0700

    create config to mysql localhost, and endpoin get id

--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,80 @@
 const express = require('express');
 const router = express();
+const { db } = require("../config/dbConfig");
 
-const dataUser = [
-    {
-        id: 1,
-        name: "Iqbal",
-        role: "User",
-    },
-];
 
-router.get("/", (req, res) => {
-    res.send(dataUser);
+router.get('/', (req, res) => {
+    const sqlQuery = "SELECT * FROM user";
+    db.query(sqlQuery, (err, result) => {
+        if (err) {
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
 });
 
 router.get("/:id", (req, res) => {
-    const param = req.params;
-    const data = dataUser.filter((item) => item.id == param.id);
-    res.send(data);
- });
+    const id = req.params.id;
+    const sqlQuery = `SELECT * FROM user WHERE user_id = ${id}`;
+    
+    db.query(sqlQuery, (err, result) => {
+        if (err) {
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
 
-router.post("/", (req, res) => {
-    const payload = req.body;
-    dataUser.push(payload);
-    res.send("Data Berhasil Ditambahkan!")
 })
 
-router.delete("/:id", (req, res) => {
-    const param = req.params;
-    const data = dataUser.filter((item) => item.id != param.id);
-    res.send(data);
-});
+// router.get("/:id", (req, res) => {
+//     const param = req.params;
+//     const data = dataUser.filter((item) => item.id == param.id);
+//     res.send(data);
+//  });
 
-router.put("/:id", (req, res) => {
-    const param = req.params;
-    const newName = req.body.name;
-    const newUsers = dataUser.map((item) => {
-        return item.id == param.id ? {...item, name: newName} : item; 
-    })
-    res.send(newUsers)
 
-});
+
+
+// const dataUser = [
+//     {
+//         id: 1,
+//         name: "Iqbal",
+//         role: "User",
+//     },
+// ];
+
+// router.get("/", (req, res) => {
+//     res.send(dataUser);
+// });
+
+// router.get("/:id", (req, res) => {
+//     const param = req.params;
+//     const data = dataUser.filter((item) => item.id == param.id);
+//     res.send(data);
+//  });
+
+// router.post("/", (req, res) => {
+//     const payload = req.body;
+//     dataUser.push(payload);
+//     res.send("Data Berhasil Ditambahkan!")
+// })
+
+// router.delete("/:id", (req, res) => {
+//     const param = req.params;
+//     const data = dataUser.filter((item) => item.id != param.id);
+//     res.send(data);
+// });
+
+// router.put("/:id", (req, res) => {
+//     const param = req.params;
+//     const newName = req.body.name;
+//     const newUsers = dataUser.map((item) => {
+//         return item.id == param.id ? {...item, name: newName} : item; 
+//     })
+//     res.send(newUsers)
+
+// });
 
 module.exports = router;
